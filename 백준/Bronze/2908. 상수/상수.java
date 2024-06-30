828fd94b4b2e10e98f0ffa9ad4d0e7bea9610b8b
yerinnn_J <86339431+YerinJi@users.noreply.github.com>
2024-06-30 14:42:09 +0900

    [Bronze II] Title: 상수, Time: 204 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        sc.close();
+
+        A = Integer.parseInt(new StringBuilder().append(A).reverse().toString());
+        B = Integer.parseInt(new StringBuilder().append(B).reverse().toString());
+        System.out.println(A < B ? B :A);
+    }
+}
