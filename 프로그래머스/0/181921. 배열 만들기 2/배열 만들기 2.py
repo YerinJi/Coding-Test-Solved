5ddbb357be8b7183c21b9a13ec1e8d53a908a9a6
yerinnn_J <86339431+YerinJi@users.noreply.github.com>
2024-08-10 21:33:46 +0900

    [level 0] Title: 배열 만들기 2, Time: 229.51 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -0,0 +1,15 @@
+def solution(l, r):
+    answer = []
+    for i in range(l,r+1):
+        check = True
+        for j in str(i):
+            if j != "5" and j != "0":
+                check = False
+                break
+        if check == True:
+            answer.append(i)
+            
+    if not answer:
+        return [-1]
+        
+    return answer
