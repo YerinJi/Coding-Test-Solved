51ff06a27e38bb6e677b5fa3c4ae6601abc6086b
yerinnn_J <86339431+YerinJi@users.noreply.github.com>
2024-08-11 10:50:59 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.py
@@ -0,0 +1,18 @@
+def solution(x1, x2, x3, x4):
+    
+    if x1 or x2:
+        first = True
+    else:
+        first = False
+        
+    if x3 or x4:
+        second = True
+    else:
+        second = False
+    
+    if first and second:
+        answer = True
+    else:
+        answer = False
+    
+    return answer
